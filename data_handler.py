a00e0877289bab5aa315c8af4fd40ff98d1509e4
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-01-18 20:34:55 +0100

    implement 'List All User Stories'

--- data_handler.py
+++ data_handler.py
@@ -1,10 +1,17 @@
 import csv
 import os
 
-DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
+#DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
+DATA_FILE_PATH = 'test/data.csv'
 DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
 STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
 
 def get_all_user_story():
-    return []
+    user_stories = []
+    with open(DATA_FILE_PATH) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            user_stories.append(row)
+    return user_stories
+
--- server.py
+++ server.py
@@ -8,9 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    user_stories = data_handler.get_all_user_story()
+    all_user_stories = data_handler.get_all_user_story()
 
-    return render_template('list.html', user_stories=user_stories)
+    return render_template('list.html', user_stories=all_user_stories)
+
+
+@app.route('/story')
+def new_story():
+    return render_template('story.html')
+
 
 if __name__ == '__main__':
     app.run(
