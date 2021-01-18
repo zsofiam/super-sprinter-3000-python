36ecb398b74ac46ad1aa8549e9af1829044a7ae5
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-01-18 23:14:15 +0100

    finish project

--- data_handler.py
+++ data_handler.py
@@ -2,10 +2,10 @@ import csv
 import os
 
 #DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
-DATA_FILE_PATH = 'test/data.csv'
+DATA_FILE_PATH = 'data.csv'
 DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
 STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
-
+LATEST_ID_PATH = 'latest_id.txt'
 
 def get_all_user_story():
     user_stories = []
@@ -14,4 +14,48 @@ def get_all_user_story():
         for row in csv_reader:
             user_stories.append(row)
     return user_stories
+
+def add_user_story(user_story):
+    user_stories = get_all_user_story()
+    user_stories.append(user_story)
+    save_user_stories_in_file(user_stories)
+
+
+def save_user_stories_in_file(user_stories):
+    f = open(DATA_FILE_PATH, "w")
+    writer = csv.DictWriter(f, DATA_HEADER)
+    writer.writeheader()
+    for story in user_stories:
+        writer.writerow(story)
+    f.close()
+
+
+def get_new_id():
+    new_id = 1
+    with open(LATEST_ID_PATH) as file:
+        new_id += int(file.read())
+    with open(LATEST_ID_PATH, 'w') as file:
+        file.write(str(new_id))
+    return new_id
+
+def get_user_story(story_id):
+    user_stories = get_all_user_story()
+    for user_story in user_stories:
+        if user_story['id'] == story_id:
+            return user_story
+    return None
+
+def delete_user_story(story_id):
+    user_stories = get_all_user_story()
+    user_stories = delete_user_story_from_list(story_id, user_stories)
+    save_user_stories_in_file(user_stories)
+
+def delete_user_story_from_list(story_id, user_stories):
+    new_user_stories = []
+    for user_story in user_stories:
+        if int(user_story["id"]) is not int(story_id):
+            new_user_stories.append(user_story)
+    return new_user_stories
+
+
 
--- server.py
+++ server.py
@@ -13,9 +13,39 @@ def route_list():
     return render_template('list.html', user_stories=all_user_stories)
 
 
-@app.route('/story')
+@app.route('/story', methods=['GET', 'POST'])
 def new_story():
+    user_story = {}
+    if request.method == 'POST':
+        user_story["title"] = request.form["story-title"]
+        user_story["user_story"] = request.form["user-story"]
+        user_story["acceptance_criteria"] = request.form["acceptance-criteria"]
+        user_story["business_value"] = request.form["business-value"]
+        user_story["estimation"] = request.form["estimation"]
+        user_story["id"] = data_handler.get_new_id()
+        user_story["status"] = 'planning'
+        data_handler.add_user_story(user_story)
+        return redirect('/')
+
     return render_template('story.html')
+
+
+@app.route("/story/<story_id>", methods=['GET', 'POST', 'PUT'])
+def edit_story(story_id):
+    user_story = data_handler.get_user_story(story_id)
+    if request.method == 'POST':
+        data_handler.delete_user_story(story_id)
+        user_story["title"] = request.form["story-title"]
+        user_story["user_story"] = request.form["user-story"]
+        user_story["acceptance_criteria"] = request.form["acceptance-criteria"]
+        user_story["business_value"] = request.form["business-value"]
+        user_story["estimation"] = request.form["estimation"]
+        user_story["id"] = data_handler.get_new_id()
+        user_story["status"] = request.form["status"]
+        data_handler.add_user_story(user_story)
+        return redirect('/')
+    story_statuses = data_handler.STATUSES
+    return render_template('story.html', story=user_story, statuses=story_statuses)
 
 
 if __name__ == '__main__':
